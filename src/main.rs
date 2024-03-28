eba2ae6abf7496bf3a8c777b9d5b5cbacb26f8a8
mslalith <mslalith.opensource@gmail.com>
2024-03-28 10:27:11 +0530

    stage #5

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
 };
+
+use itertools::Itertools;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -11,20 +13,40 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let mut buf = String::new();
-                let mut buf_reader = BufReader::new(stream.try_clone().unwrap());
-                buf_reader.read_line(&mut buf).unwrap();
-                println!("{buf}");
-                let s = buf.split(' ').collect::<Vec<_>>()[1];
-                println!("s = {s}");
-                if s == "/" {
+                let mut buf = [0; 512];
+                // let mut buf = String::new();
+                stream.read(&mut buf).unwrap();
+                // let mut buf_reader = BufReader::new(stream.try_clone().unwrap());
+                // buf_reader.read_line(&mut buf).unwrap();
+                let request = std::str::from_utf8(&buf).unwrap();
+                println!("request: {request}");
+                let mut lines = request.split("\r\n");
+                let first_line = lines.nth(0).unwrap();
+                let first = first_line.split(" ").collect::<Vec<_>>()[1];
+                // println!("first_line = {first_line}");
+                if first == "/" {
                     respond_ok(&mut stream, None, None);
-                } else if let Some(rest) = s.strip_prefix("/echo/") {
+                } else if let Some(rest) = first.strip_prefix("/echo/") {
                     respond_ok(
                         &mut stream,
                         Some(ContentType::TEXT_PLAIN),
                         Some(rest.to_owned()),
                     );
+                } else if first.starts_with("/user-agent") {
+                    let mut body: Option<&str> = None;
+                    for line in lines {
+                        if line.starts_with("User-Agent: ") {
+                            body = Some(line.split("User-Agent: ").collect::<Vec<_>>()[1]);
+                        }
+                    }
+                    match body {
+                        Some(body) => respond_ok(
+                            &mut stream,
+                            Some(ContentType::TEXT_PLAIN),
+                            Some(body.to_owned()),
+                        ),
+                        None => respond_not_found(&mut stream),
+                    };
                 } else {
                     respond_not_found(&mut stream);
                 }
