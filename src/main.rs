dfcb40a3c1bf2fd6c9adb405009294a4dfa7b580
mslalith <mslalith.opensource@gmail.com>
2024-03-28 09:08:51 +0530

    stage #2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream
+                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                    .expect("Failed to write to stream");
                 println!("accepted new connection");
             }
             Err(e) => {
