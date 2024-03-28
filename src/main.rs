37852522c60338cbbb96266582967824bea940e9
mslalith <mslalith.opensource@gmail.com>
2024-03-28 09:32:20 +0530

    stage #3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,10 +10,22 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream
-                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
-                    .expect("Failed to write to stream");
                 println!("accepted new connection");
+                let mut buf = String::new();
+                let mut buf_reader = BufReader::new(stream.try_clone().unwrap());
+                buf_reader.read_line(&mut buf).unwrap();
+                println!("{buf}");
+                let s = buf.split(' ').collect::<Vec<_>>()[1];
+                println!("s = {s}");
+                if s == "/" {
+                    stream
+                        .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                        .expect("Failed to write to stream");
+                } else {
+                    stream
+                        .write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                        .expect("Failed to write to stream");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
